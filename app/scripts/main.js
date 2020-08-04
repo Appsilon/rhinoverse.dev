a9de28609a07c546ec66d5cb0a0393ed1c08d9a9
Jakub Chojna <jakub.chojna@appsilon.com>
2020-08-04 16:45:59 +0200

    feat: add hexagonal svg shape and add initial hex-grid function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,16 @@
-console.log('\'Allo \'Allo!');
+const hexGrid = document.getElementById('hex-grid');
 
+const getCell = () => {
+  const cell = document.createElement('div');
+  cell.className = 'hex-grid__cell';
+  cell.innerHTML = `
+    <svg class="hex-grid__svg" viewBox="0 0 100 115.47">
+      <use href="images/vectors.svg#hex"></use>
+    </svg>
+  `;
+  return cell;
+}
+
+for(let i = 0; i < 20; i++) {
+  hexGrid.appendChild(getCell())
+}
