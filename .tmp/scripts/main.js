a08a3aaa8847c5907951fc0ebfd3f50954d4c3d0
Jakub Chojna <jakub.chojna@appsilon.com>
2020-08-05 10:54:33 +0200

    feat: generate hexagonal grid based on data

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,23 +1,24 @@
 import { desktopHexData } from './hex.js';
 const hexGrid = document.getElementById('hex-grid');
 
-const getCell = (width, height, total, i) => {
+const getCell = (hex, index) => {
+  const {
+    column,
+    row,
+    scale,
+    isAnimated,
+    icon,
+    isDetached
+  } = hex;
   const cell = document.createElement('div');
-  cell.className = 'hex-grid__cell';
+  cell.className = 'hex-grid__cell'; //cell.style.padding = `${1 - scale}%`;
+
+  cell.style.width = row % 2 === 0 ? `${1 / 8 * 100}%` : `${1 / 7 * 100}%`;
   cell.innerHTML = `
     <svg class="hex-grid__svg" viewBox="0 0 100 115.47">
       <use href="images/vectors.svg#hex"></use>
     </svg>
   `;
-  cell.style.width = `${width}px`;
-  cell.style.height = `${height}px`;
-  cell.style.margin = `${-1}px`;
-  cell.style.marginTop = `${-0.25 * height}px`;
-
-  if (i === 7) {
-    cell.style.marginLeft = `${-0.5 * width}px`;
-  }
-
   return cell;
 };
 
@@ -25,8 +26,31 @@ const totalInRow = 7;
 const containerWidth = hexGrid.clientWidth;
 const cellWidth = containerWidth / totalInRow;
 const cellHeight = cellWidth * 1.1547;
+const factor = desktopHexData.filter(hex => hex.row === 1);
+desktopHexData.forEach((hex, index) => {
+  const {
+    column,
+    row,
+    scale,
+    isAnimated,
+    icon,
+    isDetached
+  } = hex; // create new row
 
-for (let i = 0; i < 20; i++) {
-  hexGrid.appendChild(getCell(cellWidth, cellHeight, totalInRow, i));
-}
+  if (column === 1) {
+    const newRow = document.createElement('div');
+    newRow.className = 'hex-grid__row';
+    newRow.style.marginTop = `${-4.2}%`;
+
+    if (row % 2 === 0) {
+      newRow.style.width = `${8 / 7 * 100}%`;
+      newRow.style.marginLeft = `${-1 / 14 * 100}%`;
+    }
+
+    hexGrid.appendChild(newRow);
+  }
+
+  const lastRow = hexGrid.lastElementChild;
+  lastRow.appendChild(getCell(hex, index));
+});
 //# sourceMappingURL=main.js.map
