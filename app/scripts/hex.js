1f0ccf10a498f770c3684eae6794662d34d7c751
Jakub Chojna <jakub.chojna@appsilon.com>
2020-08-05 09:20:56 +0200

    feat: add array of object to define each hex-cell properties

---
+++ app/scripts/hex.js
@@ -0,0 +1,298 @@
+export const desktopHexData = [
+  {
+    column: 1,
+    row: 1,
+    scale: 0.7,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 1,
+    scale: 0.8,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 1,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 1,
+    scale: 1,
+    isAnimated: false,
+    icon: null,
+    isDetached: false
+  },
+  {
+    column: 5,
+    row: 1,
+    scale: 1,
+    isAnimated: false,
+    icon: null,
+    isDetached: false
+  },
+  {
+    column: 6,
+    row: 1,
+    scale: 1,
+    isAnimated: false,
+    icon: null,
+    isDetached: false
+  },
+  {
+    column: 7,
+    row: 1,
+    scale: 1,
+    isAnimated: false,
+    icon: null,
+    isDetached: false
+  },
+  {
+    column: 1,
+    row: 2,
+    scale: 0.7,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 2,
+    scale: 0.8,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 2,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 2,
+    scale: 1,
+    isAnimated: false,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 2,
+    scale: 0.8,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 2,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 2,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 8,
+    row: 2,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 1,
+    row: 3,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 3,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 3,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 3,
+    scale: 0.95,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 3,
+    scale: 0.8,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 3,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 3,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 1,
+    row: 4,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 4,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 4,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 4,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 4,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 4,
+    scale: 0.8,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 4,
+    scale: 0.85,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 8,
+    row: 4,
+    scale: 0.7,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 1,
+    row: 5,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 5,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 5,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 5,
+    scale: 1,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 5,
+    scale: 0.9,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 5,
+    scale: 0.8,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 5,
+    scale: 0.7,
+    isAnimated: true,
+    icon: null,
+    isDetached: true
+  }
+];
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
+import { desktopHexData } from './hex.js';
+
 const hexGrid = document.getElementById('hex-grid');
 
-const getCell = () => {
+const getCell = (width, height, total, i) => {
   const cell = document.createElement('div');
   cell.className = 'hex-grid__cell';
   cell.innerHTML = `
@@ -8,9 +10,24 @@ const getCell = () => {
       <use href="images/vectors.svg#hex"></use>
     </svg>
   `;
+  cell.style.width = `${width}px`;
+  cell.style.height = `${height}px`;
+  cell.style.margin = `${-1}px`;
+  cell.style.marginTop = `${-0.25 * height}px`;
+
+  if (i === 7) {
+    cell.style.marginLeft = `${-0.5 * width}px`;
+  }
+
   return cell;
 }
 
+const totalInRow = 7;
+const containerWidth = hexGrid.clientWidth;
+const cellWidth = containerWidth / totalInRow;
+const cellHeight = cellWidth * 1.1547;
+
 for(let i = 0; i < 20; i++) {
-  hexGrid.appendChild(getCell())
+  hexGrid.appendChild(getCell(cellWidth, cellHeight, totalInRow, i))
 }
+
