41ce2d1b9560f575914213d7179358a257ed351e
Jakub Chojna <jakub.chojna@appsilon.com>
2020-08-06 10:56:03 +0200

    feat: update package.json and gulp configuration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,303 @@
-import { desktopHexData } from './hex.js';
+//import {desktopHexData} from './hex.js';
+const desktopHexData = [
+  {
+    column: 1,
+    row: 1,
+    gap: 1,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 1,
+    gap: 1,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 1,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 1,
+    gap: 0,
+    isAnimated: false,
+    iconId: null,
+    isDetached: false
+  },
+  {
+    column: 5,
+    row: 1,
+    gap: 0,
+    isAnimated: false,
+    iconId: null,
+    isDetached: false
+  },
+  {
+    column: 6,
+    row: 1,
+    gap: 0,
+    isAnimated: false,
+    iconId: null,
+    isDetached: false
+  },
+  {
+    column: 7,
+    row: 1,
+    gap: 0,
+    isAnimated: false,
+    iconId: null,
+    isDetached: false
+  },
+  {
+    column: 1,
+    row: 2,
+    gap: 1,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 2,
+    gap: 0.6,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 2,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 2,
+    gap: 0,
+    isAnimated: false,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 2,
+    gap: 0.6,
+    isAnimated: true,
+    iconId: 'semantic-dashboard',
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 2,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 2,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 8,
+    row: 2,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 1,
+    row: 3,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 3,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 3,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 3,
+    gap: 0.35,
+    isAnimated: true,
+    iconId: 'shiny-semantic',
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 3,
+    gap: 0.6,
+    isAnimated: true,
+    iconId: 'shiny-router',
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 3,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 3,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 1,
+    row: 4,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 4,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 4,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 4,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 4,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: 'shiny-info',
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 4,
+    gap: 0.6,
+    isAnimated: true,
+    iconId: 'shiny-i18n',
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 4,
+    gap: 0.65,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 8,
+    row: 4,
+    gap: 1,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 1,
+    row: 5,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 2,
+    row: 5,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 3,
+    row: 5,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 4,
+    row: 5,
+    gap: 0,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 5,
+    row: 5,
+    gap: 0.3,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 6,
+    row: 5,
+    gap: 0.6,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  },
+  {
+    column: 7,
+    row: 5,
+    gap: 1,
+    isAnimated: true,
+    iconId: null,
+    isDetached: true
+  }
+];
+
 const hexGrid = document.getElementById('hex-grid');
 const totalInBigRow = desktopHexData.reduce((max, curr) =>
   curr.column > max.column ? curr : max).column;
@@ -34,7 +333,7 @@ const getCell = (hex, totalInBigRow, totalInSmallRow) => {
     cell.innerHTML += `
       <div class="cell__label">
         <svg class="cell__logo" viewBox="0 0 100 100">
-          <use href="images/vectors.svg#${iconId}"></use>
+          <use href="svg/vectors.svg#${iconId}"></use>
         </svg>
         <p class="cell__title">${iconId}</p>
       </div>
