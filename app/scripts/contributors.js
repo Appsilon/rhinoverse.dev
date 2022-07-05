c16b0bf1e098f5a2ba3dd88ee7efa80b68e7ee48
Jakub Chojna <jakub.chojna@appsilon.com>
2022-07-05 19:40:49 +0200

    feat: add contributors to rhino

--- app/scripts/contributors.js
+++ app/scripts/contributors.js
@@ -9,73 +9,79 @@ export const addContributors = () => {
 
   const credits = {
     'shiny.semantic': [
-        {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Creator'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
-        {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
-        {'img': '4321346', 'profile': 'olgamie', 'credit': 'Contributor'},
-        {'img': '8420419', 'profile': 'ashbaldry', 'credit': 'Contributor'},
-        {'img': '47003650', 'profile': 'jchojna', 'credit': 'Contributor'},
-        {'img': '24652240', 'profile': 'Appsilon/shiny.semantic/graphs/contributors', 'credit': 'More Contributors'}
-      ],
+      {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Creator'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
+      {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
+      {'img': '4321346', 'profile': 'olgamie', 'credit': 'Contributor'},
+      {'img': '8420419', 'profile': 'ashbaldry', 'credit': 'Contributor'},
+      {'img': '47003650', 'profile': 'jchojna', 'credit': 'Contributor'},
+      {'img': '24652240', 'profile': 'Appsilon/shiny.semantic/graphs/contributors', 'credit': 'More Contributors'}
+    ],
     'semantic.dashboard': [
-        {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Creator'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
-        {'img': '26330688', 'profile': 'drzamich', 'credit': 'Contributor'},
-        {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
-        {'img': '11685462', 'profile': 'maju116', 'credit': 'Contributor'},
-        {'img': '4296390', 'profile': 'galachad', 'credit': 'Contributor'},
-        {'img': '24652240', 'profile': 'Appsilon/semantic.dashboard/graphs/contributors', 'credit': 'More Contributors'}
-      ],
+      {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Creator'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
+      {'img': '26330688', 'profile': 'drzamich', 'credit': 'Contributor'},
+      {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
+      {'img': '11685462', 'profile': 'maju116', 'credit': 'Contributor'},
+      {'img': '4296390', 'profile': 'galachad', 'credit': 'Contributor'},
+      {'img': '24652240', 'profile': 'Appsilon/semantic.dashboard/graphs/contributors', 'credit': 'More Contributors'}
+    ],
     'shiny.worker': [
-        {'img': '4669074', 'profile': 'przytu1', 'credit': 'Creator'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Creator'}
-      ],
+      {'img': '4669074', 'profile': 'przytu1', 'credit': 'Creator'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Creator'}
+    ],
     'shiny.info': [
-        {'img': '7279715', 'profile': 'jakubnowicki', 'credit': 'Creator & Maintainer'},
-        {'img': '4669074', 'profile': 'przytu1', 'credit': 'Creator'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
-        {'img': '19154385', 'profile': 'mdubel', 'credit': 'Contributor'},
-        {'img': '15175871', 'profile': 'rabiibouhestine', 'credit': 'Contributor'},
-        {'img': '43342554', 'profile': 'FractalPolarity', 'credit': 'Contributor'}
-      ],
+      {'img': '7279715', 'profile': 'jakubnowicki', 'credit': 'Creator & Maintainer'},
+      {'img': '4669074', 'profile': 'przytu1', 'credit': 'Creator'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
+      {'img': '19154385', 'profile': 'mdubel', 'credit': 'Contributor'},
+      {'img': '15175871', 'profile': 'rabiibouhestine', 'credit': 'Contributor'},
+      {'img': '43342554', 'profile': 'FractalPolarity', 'credit': 'Contributor'}
+    ],
     'shiny.router': [
-        {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Creator'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
-        {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
-        {'img': '3685123', 'profile': 'rstammer', 'credit': 'Contributor'},
-        {'img': '4090570', 'profile': 'KrzysztofWrobel', 'credit': 'Contributor'},
-        {'img': '265127', 'profile': 'agwells', 'credit': 'Contributor'},
-        {'img': '24652240', 'profile': 'Appsilon/shiny.router/graphs/contributors', 'credit': 'More Contributors'}
-      ],
+      {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Creator'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
+      {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
+      {'img': '3685123', 'profile': 'rstammer', 'credit': 'Contributor'},
+      {'img': '4090570', 'profile': 'KrzysztofWrobel', 'credit': 'Contributor'},
+      {'img': '265127', 'profile': 'agwells', 'credit': 'Contributor'},
+      {'img': '24652240', 'profile': 'Appsilon/shiny.router/graphs/contributors', 'credit': 'More Contributors'}
+    ],
     'shiny.i18n': [
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Creator'},
-        {'img': '37193264', 'profile': 'jakubsob', 'credit': 'Maintainer'},
-        {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
-        {'img': '13157213', 'profile': 'krzyslom', 'credit': 'Contributor'},
-        {'img': '24652240', 'profile': 'Appsilon/shiny.i18n/graphs/contributors', 'credit': 'More Contributors'}
-      ],
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Creator'},
+      {'img': '37193264', 'profile': 'jakubsob', 'credit': 'Maintainer'},
+      {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Contributor'},
+      {'img': '13157213', 'profile': 'krzyslom', 'credit': 'Contributor'},
+      {'img': '24652240', 'profile': 'Appsilon/shiny.i18n/graphs/contributors', 'credit': 'More Contributors'}
+    ],
     'shiny.react': [
-        {'img': '10000651', 'profile': 'kamilzyla', 'credit': 'Creator & Maintainer'},
-        {'img': '1421503', 'profile': 'marekrogala', 'credit': 'Creator'},
-        {'img': '37193264', 'profile': 'jakubsob', 'credit': 'Maintainer'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'}
-      ],
+      {'img': '10000651', 'profile': 'kamilzyla', 'credit': 'Creator & Maintainer'},
+      {'img': '1421503', 'profile': 'marekrogala', 'credit': 'Creator'},
+      {'img': '37193264', 'profile': 'jakubsob', 'credit': 'Maintainer'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'}
+    ],
     'shiny.fluent': [
-        {'img': '10000651', 'profile': 'kamilzyla', 'credit': 'Creator & Maintainer'},
-        {'img': '1421503', 'profile': 'marekrogala', 'credit': 'Creator'},
-        {'img': '37193264', 'profile': 'jakubsob', 'credit': 'Maintainer'},
-        {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Contributor'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
-        {'img': '32652297', 'profile': 'jimbrig', 'credit': 'Contributor'}
-      ],
+      {'img': '10000651', 'profile': 'kamilzyla', 'credit': 'Creator & Maintainer'},
+      {'img': '1421503', 'profile': 'marekrogala', 'credit': 'Creator'},
+      {'img': '37193264', 'profile': 'jakubsob', 'credit': 'Maintainer'},
+      {'img': '4673386', 'profile': 'filipstachura', 'credit': 'Contributor'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
+      {'img': '32652297', 'profile': 'jimbrig', 'credit': 'Contributor'}
+    ],
     'data.validator': [
-        {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Creator'},
-        {'img': '19154385', 'profile': 'mdubel', 'credit': 'Creator & Maintainer'},
-        {'img': '11685462', 'profile': 'maju116', 'credit': 'Contributor'},
-        {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
-        {'img': '4669074', 'profile': 'przytu1', 'credit': 'Contributor'},
-        {'img': '4244169', 'profile': 'FrieseWoudloper', 'credit': 'Contributor'}
-      ]
+      {'img': '20457043', 'profile': 'krystian8207', 'credit': 'Creator'},
+      {'img': '19154385', 'profile': 'mdubel', 'credit': 'Creator & Maintainer'},
+      {'img': '11685462', 'profile': 'maju116', 'credit': 'Contributor'},
+      {'img': '4547289', 'profile': 'dokato', 'credit': 'Contributor'},
+      {'img': '4669074', 'profile': 'przytu1', 'credit': 'Contributor'},
+      {'img': '4244169', 'profile': 'FrieseWoudloper', 'credit': 'Contributor'}
+    ],
+    'rhino': [
+      {'img': '10000651', 'profile': 'kamilzyla', 'credit': 'Creator & Maintainer'},
+      {'img': '7279715', 'profile': 'jakubnowicki', 'credit': 'Contributor'},
+      {'img': '38053499', 'profile': 'TymekDev', 'credit': 'Contributor'},
+      {'img': '1421503', 'profile': 'marekrogala', 'credit': 'Contributor'}
+    ]
   };
 
   Object.keys(credits).forEach(tool => {
